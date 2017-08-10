7268a4a2c750006326c33c1aea70e329a6024d89
AZReed <azreed@gmail.com>
2017-08-10 19:55:43 -0400

    feat: Reading books to show in main page
    A first attempt to implement Book API. Not sure if I like calling
    BookShelf 3 times.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import SearchNav from './SearchNav'
 import BookShelf from './BookShelf'
 import './App.css'
@@ -13,17 +14,55 @@ class BooksApp extends Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then( books => {
+      this.setState({ books })
+    }) 
+  }
+
+  filterBooksByShelf = shelf => {
+    return this.state.books.filter( book =>
+      book.shelf === shelf
+    )
   }
 
   render() {
 
+    // const {books} = this.state
+
     return (
       <div className="app">
+
         <Route exact path="/search" render={ () => (
           <SearchNav />
         )}/>
+
         <Route exact path="/" render={ () => (
-          <BookShelf/>
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <BookShelf
+                  shelfName="Currently Reading"
+                  books={this.filterBooksByShelf('currentlyReading')}
+                />
+                <BookShelf
+                  shelfName="Want To Read"
+                  books={this.filterBooksByShelf('wantToRead')}
+                />
+                <BookShelf
+                  shelfName="Read"
+                  books={this.filterBooksByShelf('read')}
+                />
+              </div>
+            <div className="open-search">
+              <Link to="/search">Add a book</Link>
+            </div>
+          </div>
         )}/>
       </div>
     )
