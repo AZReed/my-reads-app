ad40fdc92c8777ec05aaa05b5d4d1ab7272fda63
AZReed <azreed@gmail.com>
2017-08-17 15:11:12 -0300

    refactor: validation for imageLink missing

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class Book extends Component {
 
+  handleImage = () => {
+    if (this.props.book.imageLinks === undefined) {
+      return { width: 128, height: 193, backgroundImage: `url(http://tendertiger.com/images/NoDataFound.png)`, backgroundSize: 'cover' }
+    }
+    return { width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})` }
+  }
+
   render() {
 
     const book = this.props.book
@@ -10,7 +17,8 @@ class Book extends Component {
       <li key={book.id}>
         <div className="book">
           <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          {/* <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div> */}
+          <div className="book-cover" style={this.handleImage()}></div>
           <div className="book-shelf-changer">
             <select value={book.shelf || 'none'} onChange={this.props.handleChange.bind(this,book)}>
             <option disabled>Move to...</option>
